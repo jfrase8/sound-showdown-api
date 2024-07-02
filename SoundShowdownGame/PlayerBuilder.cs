7df1d6542cf21e1946c98025f6bf3c3964750b00
Thomas Kendall <thomas.j.kendall@gmail.com>
2024-07-02 09:11:02 -0500

    Added builder classes

---
+++ SoundShowdownGame/PlayerBuilder.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundShowdownGame
+{
+    public class PlayerBuilder
+    {
+        private static int IdCounter = 1;
+
+        private string Id;
+        private GenreName? Genre;
+        private int Health;
+        private List<Instrument> Instruments;
+        private Dictionary<Resource, int> InventoryResources;
+        private Dictionary<Resource, int> InventoryAccumulatedResources;
+        private int InventoryCoins;
+        private Enemy? Enemy;
+
+        public PlayerBuilder() 
+        {
+            // Defaults
+            Id = $"Player_{IdCounter++}";
+            Genre = null;
+            Health = 10;
+            Instruments = new List<Instrument>();
+            InventoryResources = new Dictionary<Resource, int>();
+            InventoryAccumulatedResources = new Dictionary<Resource, int>();
+            InventoryCoins = 0;
+            Enemy = null;
+        }
+
+        public Player Build()
+        {
+            // TODO: Create an InventoryBuilder class
+            Inventory inventory = new Inventory();
+            inventory.ResourceInventory = InventoryResources;
+            inventory.AccumulatedResources = InventoryAccumulatedResources;
+            inventory.Coins = InventoryCoins;
+
+            Player player = new Player(id: Id, genre: Genre, health: Health, instruments: Instruments, inventory: inventory, enemy: Enemy);
+            return player;
+        }
+
+        public PlayerBuilder WithId(string id)
+        {
+            Id = id;
+            return this;
+        }
+
+        public PlayerBuilder WithGenre(GenreName? genre)
+        {
+            Genre = genre;
+            return this;
+        }
+
+        public PlayerBuilder WithHealth(int health)
+        {
+            Health = health;
+            return this;
+        }
+
+        public PlayerBuilder WithInstrument(Instrument instrument)
+        {
+            Instruments.Add(instrument);
+            return this;
+        }
+
+        public PlayerBuilder WithInventoryResource(Resource resource, int amount)
+        {
+            InventoryResources[resource] = amount;
+            return this;
+        }
+
+        public PlayerBuilder WithInventoryAccumulatedResource(Resource resource, int amount)
+        {
+            InventoryAccumulatedResources[resource] = amount;
+            return this;
+        }
+
+        public PlayerBuilder WithInventoryCoins(int coins)
+        {
+            InventoryCoins = coins;
+            return this;
+        }
+
+        public PlayerBuilder WithEnemy(Enemy? enemy)
+        {
+            Enemy = enemy;
+            return this;
+        }
+
+    }
+}
---
+++ SoundShowdownGame/SoundShowdownBuilder.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundShowdownGame
+{
+    public class SoundShowdownBuilder
+    {
+        private List<Player> Players;
+        private Deck<Enemy>? EnemyDeck;
+        private int ActionsCount;
+        private GameState CurrentGameState;
+        private int EnemiesDefeated;
+        private Enemy? CurrentEnemy;
+
+        public SoundShowdownBuilder() 
+        {
+            // Create defaults
+            Players = new List<Player>();
+            EnemyDeck = null;
+            ActionsCount = 3;
+            CurrentGameState = GameState.Awaiting_Player_Choose_Genre;
+            EnemiesDefeated = 0;
+            CurrentEnemy = null;
+        }
+
+        public SoundShowdown Build()
+        {
+            // Validate
+            if(Players.Count < 2)
+            {
+                throw new Exception("Must have at least 2 players to create an instance of SoundShowdown.");
+            }
+
+            // TODO: Finish validation
+
+            // Create a deck if one has not been defined
+            if (EnemyDeck == null)
+            {
+                EnemyDeck = EnemyDeckFactory.CreateShuffledDeck();
+            }
+
+            SoundShowdown game = new SoundShowdown(players: Players, enemyDeck: EnemyDeck, actionsCount: ActionsCount, currentGameState: CurrentGameState, enemiesDefeated: EnemiesDefeated, currentEnemy: CurrentEnemy);
+            return game;
+        }
+
+        public SoundShowdownBuilder WithPlayer(Player player)
+        {
+            Players.Add(player);
+            return this;
+        }
+
+        public SoundShowdownBuilder WithEnemyDeck(Deck<Enemy> enemyDeck)
+        {
+            EnemyDeck = enemyDeck;
+            return this;
+        }
+
+        public SoundShowdownBuilder WithActionsCount(int actionsCount)
+        {
+            ActionsCount = actionsCount;
+            return this;
+        }
+
+        public SoundShowdownBuilder WithCurrentGameState(GameState currentGameState)
+        {
+            CurrentGameState = currentGameState;
+            return this;
+        }
+
+        public SoundShowdownBuilder WithEnemiesDefeated(int enemiesDefeated)
+        {
+            EnemiesDefeated = enemiesDefeated;
+            return this;
+        }
+
+        public SoundShowdownBuilder WithCurrentEnemy(Enemy? currentEnemy)
+        {
+            CurrentEnemy = currentEnemy;
+            return this;
+        }
+
+    }
+}
