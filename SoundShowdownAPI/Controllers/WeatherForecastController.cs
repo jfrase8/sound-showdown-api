ef4cbe8fbce6ca3690942c45e2473cfbd41ccdb2
Thomas Kendall <thomas.j.kendall@gmail.com>
2024-08-19 14:46:28 -0500

    Added sunny

--- SoundShowdownAPI/Controllers/WeatherForecastController.cs
+++ SoundShowdownAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace SoundShowdownAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Sunny"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
